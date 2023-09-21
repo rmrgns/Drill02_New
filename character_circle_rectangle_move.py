4f0e1490ea0a71b9d6019388884768fc1c37d189
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-09-21 10:13:53 +0900

    Complete run_circle
    원운동 함수 완성

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 grass = load_image('grass.png')
@@ -6,11 +7,15 @@ character = load_image('character.png')
 
 def run_circle():
     print('Circle')
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(400, 90)
-    delay(1)
-    
+
+    cx, cy, r = 400, 300, 200
+    for deg in range(0, 360, 1):
+        x = cx + r * math.cos(math.radians(deg))
+        y = cy + r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.01)
     pass
 
 def run_rectangle():
